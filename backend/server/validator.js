72764bc5538cb1bc6bed6411e0eed09836116e1e
Joey Nguyen <joeylnguyen95@gmail.com>
2020-11-30 20:23:21 -0800

    Implement express-validator's native validator methods such as isPostalCode and isCreditCard

--- backend/server/validator.js
+++ backend/server/validator.js
@@ -1,58 +1,61 @@
-const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require('express-validator');
 const {
-  validateCardNumber,
   validateCardExpiration,
   validateZipCode,
   validatePhoneNumber,
   validateCardExpirationFormat,
-} = require("../../client/src/components/App/utils/validators");
+} = require('../../client/src/components/App/utils/validators');
 
 const userValidationRules = () => {
   return [
-    body("email", "Email address is required").trim().notEmpty(),
-    body("email", "Please enter a valid email address")
-      .if(body("email").notEmpty())
+    body('email', 'Email address is required').trim().notEmpty(),
+    body('email', 'Please enter a valid email address')
+      .if(body('email').notEmpty())
       .isEmail()
       .normalizeEmail(),
     // TODO: Add custom check for if email already exists in DB
-    body("quantity", "Max quantity for this item is 3").custom(
+    body('quantity', 'Max quantity for this item is 3').custom(
       (value) => value <= 3
     ),
     // TODO: Add custom check for if user purchased more than 3 already
-    body("total").isString().notEmpty(),
-    body("firstName", "First name is required").trim().notEmpty().escape(),
-    body("lastName", "Last name is required").trim().notEmpty().escape(),
-    body("phone", "Phone number is required").trim().notEmpty().escape(),
-    body("phone", "Please enter a valid 10-digit phone number")
-      .if(body("phone").notEmpty())
+    body('total').isString().notEmpty(),
+    body('firstName', 'First name is required').trim().notEmpty().escape(),
+    body('lastName', 'Last name is required').trim().notEmpty().escape(),
+    body('phone', 'Phone number is required').trim().notEmpty().escape(),
+    body('phone', 'Please enter a valid 10-digit phone number')
+      .if(body('phone').notEmpty())
       .custom((value) => validatePhoneNumber(value)),
-    body("address.city", "City is required").trim().notEmpty().escape(),
-    body("address.state", "State is required").trim().notEmpty().escape(),
-    body("address.street1", "Address line 1 is required")
+    body('address.city', 'City is required').trim().notEmpty().escape(),
+    body('address.state', 'State is required').trim().notEmpty().escape(),
+    body('address.street1', 'Address line 1 is required')
+      .trim()
+      .isLength({ max: 50, min: 6 })
+      .escape(),
+    body('address.street2')
+      .optional({ checkFalsy: true })
+      .isLength({ max: 50, min: 6 })
+      .trim()
+      .escape(),
+    body('address.zipCode', 'Zip code is required').trim().notEmpty().escape(),
+    body('address.zipCode', 'Please enter a valid zip code')
+      .if(body('address.zipCode').notEmpty())
+      .isPostalCode('US'),
+    body('payment.ccNum', 'Credit card number is required')
       .trim()
       .notEmpty()
       .escape(),
-    body("address.street2").trim().escape(),
-    body("address.zipCode", "Zip code is required").trim().notEmpty().escape(),
-    body("address.zipCode", "Please enter a valid zip code")
-      .if(body("address.zipCode").notEmpty())
-      .custom((value) => validateZipCode(value)),
-    body("payment.ccNum", "Credit card number is required")
-      .trim()
-      .notEmpty()
-      .escape(),
-    body("payment.ccNum", "Please enter a valid credit card number")
-      .if(body("payment.ccNum").notEmpty())
-      .custom((value) => validateCardNumber(value)),
-    body("payment.exp", "Credit card expiration date is required")
+    body('payment.ccNum', 'Please enter a valid credit card number')
+      .if(body('payment.ccNum').notEmpty())
+      .isCreditCard(),
+    body('payment.exp', 'Credit card expiration date is required')
       .trim()
       .isLength({ min: 5 }),
-    body("payment.exp", "Invalid credit card expiration date format")
-      .if(body("payment.exp").isLength({ max: 5 }))
+    body('payment.exp', 'Invalid credit card expiration date format')
+      .if(body('payment.exp').isLength({ max: 5 }))
       .custom((value) => validateCardExpirationFormat(value)),
-    body("payment.exp", "Credit card is expired")
+    body('payment.exp', 'Credit card is expired')
       .if(
-        body("payment.exp").custom((value) =>
+        body('payment.exp').custom((value) =>
           validateCardExpirationFormat(value)
         )
       )
