49d920ebc0f56f258a3762939f876975d0c61d34
Joey Nguyen <joeylnguyen95@gmail.com>
2020-11-30 20:58:53 -0800

    Set up axios post request to send form data to server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { check, validationResult, oneOf, body } = require("express-validator");
-const { userValidationRules, validate } = require("./validator");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { check, validationResult, oneOf, body } = require('express-validator');
+const { userValidationRules, validate } = require('./validator');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello!"));
+app.get('/', (req, res) => res.send('Hello!'));
 
-app.post("/api/magic", userValidationRules(), validate, (req, res) => {
+app.post('/api/magic', userValidationRules(), validate, (req, res) => {
   res.sendStatus(201);
 });
 
